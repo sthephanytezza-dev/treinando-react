7fa4fe7e51bb0d1bfbe6b13af3da52ef43a31912
sthephanytezza-dev <sthephany.tezza@rethink.dev>
2022-05-09 16:22:24 -0300

    imagens públicas

---
+++ avancado/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Avançado em react</h1>
+      {/* Imagem em public*/}
+      <div>
+        <img src="/img1.jpg" alt="Paisagem" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
