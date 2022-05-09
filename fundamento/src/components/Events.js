eec0a1e5a5940bb3637213c0eccc1adbeb99cd1a
sthephanytezza-dev <sthephany.tezza@rethink.dev>
2022-05-09 15:19:20 -0300

    função de renderização

--- fundamento/src/components/Events.js
+++ fundamento/src/components/Events.js
@@ -1,15 +1,28 @@
 const Events = () => {
   const handleMyEvent = (e) => {
-      console.log(e);
+    console.log(e);
   };
+
+  const renderSomething = (x) => {
+      if(x){
+        return <h1>Renderizando isso!</h1>
+      } else{
+        return <h1>Também posso renderizar isso!</h1>
+      }
+  };
+
   return (
     <>
       <div>
         <button onClick={handleMyEvent}>Clique aqui</button>
       </div>
       <div>
-          <button onClick={() => console.log("clicou")}>Clique aqui também</button>
+        <button onClick={() => console.log("clicou")}>
+          Clique aqui também
+        </button>
       </div>
+      {renderSomething(true)}
+      {renderSomething(false)}
     </>
   );
 };
