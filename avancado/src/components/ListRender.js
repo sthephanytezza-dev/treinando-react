d3029e70b54c8f2ca2198744536d5c827c2e5e85
sthephanytezza-dev <sthephany.tezza@rethink.dev>
2022-05-09 16:52:40 -0300

    previous state

--- avancado/src/components/ListRender.js
+++ avancado/src/components/ListRender.js
@@ -3,14 +3,35 @@ import { useState } from "react";
 const ListRender = () => {
   const [list] = useState(["Sthephany", "Karol", "Matheus"]);
 
+  const [users, setUsers] = useState([
+    { id: 1, name: "sthephany", age: 21 },
+    { id: 2, name: "joao", age: 24 },
+    { id: 3, name: "flavio", age: 45 },
+  ]);
+
+  const deleteRandom = () => {
+    const randomNumber = Math.floor(Math.random() * 4);
+    setUsers((prevUsers) => {
+      return prevUsers.filter((user) => randomNumber !== user.id);
+    });
+  };
+
   return (
-    <div>
-      <ul>
-        {list.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <div>
+        <ul>
+          {list.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+        <ul>
+          {users.map((item) => (
+            <li key={item.id}>{item.name}</li>
+          ))}
+        </ul>
+        <button onClick={deleteRandom}>Delete random user</button>
+      </div>
+    </>
   );
 };
 
