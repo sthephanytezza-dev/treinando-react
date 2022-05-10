742dada63c1fe95de293f3b4f18e274ef3c4d487
sthephanytezza-dev <sthephany.tezza@rethink.dev>
2022-05-10 17:58:55 -0300

    css inline dinâmico

--- css/src/App.js
+++ css/src/App.js
@@ -1,8 +1,13 @@
+import { useState } from "react";
+
 import "./App.css";
 
 import MyComponent from "./components/MyComponent";
 
 function App() {
+  const n = 15;
+  const [name] = useState("Sthephany");
+
   return (
     <div className="App">
       <h1>React com CSS</h1>
@@ -13,6 +18,22 @@ function App() {
       <p style={{ color: "blue", padding: "25px", borderTop: "2px solid red" }}>
         Esse elemento foi estilizado de forma inline
       </p>
+
+      <h2 style={n < 10 ? { color: "purple" } : { color: "pink" }}>
+        CSS dinâmico
+      </h2>
+      <h2 style={n > 10 ? { color: "purple" } : { color: "pink" }}>
+        CSS dinâmico
+      </h2>
+      <h2
+        style={
+          name === "Sthephany"
+            ? { color: "green", backgroundColor: "#000" }
+            : null
+        }
+      >
+        CSS dinâmico
+      </h2>
     </div>
   );
 }
