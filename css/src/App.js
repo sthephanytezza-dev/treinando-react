549227899c530797a456ad52bc1225746c619ee4
sthephanytezza-dev <sthephany.tezza@rethink.dev>
2022-05-10 17:50:18 -0300

    css de componentes

--- css/src/App.js
+++ css/src/App.js
@@ -1,10 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+
+import MyComponent from "./components/MyComponent";
 
 function App() {
   return (
     <div className="App">
       <h1>React com CSS</h1>
+      <MyComponent />
+      <p>Esse parágrafo é do app.js</p>
+      <p className="my_comp_para">Esse também é do componente</p>
     </div>
   );
 }
---
+++ css/src/components/MyComponent.js
@@ -0,0 +1,12 @@
+import "./MyComponent.css";
+
+const MyComponent = () => {
+  return (
+    <div>
+      <h1>CSS de componente</h1>
+      <p>Esse é o paragrafo de componente</p>
+    </div>
+  );
+};
+
+export default MyComponent;
