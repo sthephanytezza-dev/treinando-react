7d0fc0af5c6e784e1031004b3581950a1d13fbc0
sthephanytezza-dev <sthephany.tezza@rethink.dev>
2022-05-10 18:35:00 -0300

    alteração de state inline

--- form/src/components/MyForm.js
+++ form/src/components/MyForm.js
@@ -9,6 +9,7 @@ const MyForm = () => {
   const handleName = (e) => {
     setName(e.target.value);
   };
+  console.log(email);
 
   return (
     <div>
@@ -24,7 +25,12 @@ const MyForm = () => {
         </div>
         <label>
           <span>E-mail:</span>
-          <input type="email" name="email" placeholder="Digite seu email" />
+          <input
+            type="email"
+            name="email"
+            placeholder="Digite seu email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </label>
         <input type="text" value="Enviar" />
       </form>
