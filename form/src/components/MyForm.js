5afff110f87a0a73394517afdb5731f1e41b592e
sthephanytezza-dev <sthephany.tezza@rethink.dev>
2022-05-10 18:32:48 -0300

    gerenciamento de dados de input

--- form/src/components/MyForm.js
+++ form/src/components/MyForm.js
@@ -1,12 +1,26 @@
 import "./MyForm.css";
 
+import { useState } from "react";
+
 const MyForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+
   return (
     <div>
       <form>
         <div>
           <label htmlFor="name">Nome:</label>
-          <input type="text" name="name" placeholder="Digite seu nome" />
+          <input
+            type="text"
+            name="name"
+            placeholder="Digite seu nome"
+            onChange={handleName}
+          />
         </div>
         <label>
           <span>E-mail:</span>
