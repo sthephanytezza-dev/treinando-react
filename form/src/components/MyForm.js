ce6b24ec821e2477cacc99318d0402fc987f8d77
sthephanytezza-dev <sthephany.tezza@rethink.dev>
2022-05-10 18:27:43 -0300

    label envolvendo input

--- form/src/components/MyForm.js
+++ form/src/components/MyForm.js
@@ -8,6 +8,10 @@ const MyForm = () => {
           <label htmlFor="name">Nome:</label>
           <input type="text" name="name" placeholder="Digite seu nome" />
         </div>
+        <label>
+          <span>E-mail:</span>
+          <input type="email" name="email" placeholder="Digite seu email" />
+        </label>
         <input type="text" value="Enviar" />
       </form>
     </div>
