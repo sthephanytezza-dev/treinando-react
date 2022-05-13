a82895f84da8bcbe3875488138933a83ebd8a6a4
sthephanytezza-dev <sthephany.tezza@rethink.dev>
2022-05-13 18:39:09 -0300

    resgatando dados da api

--- httpreact/src/App.js
+++ httpreact/src/App.js
@@ -1,9 +1,32 @@
 import "./App.css";
 
+import { useState, useEffect } from "react";
+
+const url = "http://localhost:3000/products";
+
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(url);
+      const data = await res.json();
+      setProducts(data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <h1>Lista de produtos</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - R$:{product.price}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
