fd34e91f86e882e4ce325b1243f21809853f4131
sthephanytezza-dev <sthephany.tezza@rethink.dev>
2022-05-16 14:18:42 -0300

    search params

--- reactrouter/src/App.js
+++ reactrouter/src/App.js
@@ -9,6 +9,8 @@ import Navbar from "./components/Navbar";
 import Product from "./pages/Product";
 import Info from "./pages/Info";
 import NotFound from "./pages/NotFound";
+import SearchForm from "./components/SearchForm";
+import Search from "./pages/Search";
 
 function App() {
   return (
@@ -16,11 +18,13 @@ function App() {
       <h1>React Router</h1>
       <BrowserRouter>
         <Navbar />
+        <SearchForm />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/products/:id/info" element={<Info />} />
           <Route path="/products/:id" element={<Product />} />
+          <Route path="/search" element={<Search />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
