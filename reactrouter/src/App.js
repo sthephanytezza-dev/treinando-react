dea7b99ec74f94ac48419ef516900ca03b765ec6
sthephanytezza-dev <sthephany.tezza@rethink.dev>
2022-05-16 10:56:11 -0300

    navegação entre páginas

--- reactrouter/src/App.js
+++ reactrouter/src/App.js
@@ -3,14 +3,16 @@ import "./App.css";
 // 1- config react router
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./components/Home";
-import About from "./components/About";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <h1>React Router</h1>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
