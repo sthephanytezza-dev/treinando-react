c4ff488374ba66a86fd791056c028418ecf35a6c
sthephanytezza-dev <sthephany.tezza@rethink.dev>
2022-05-16 10:49:27 -0300

    configurando o react router

---
+++ reactrouter/src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+
+// 1- config react router
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./components/Home";
+import About from "./components/About";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Router</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
