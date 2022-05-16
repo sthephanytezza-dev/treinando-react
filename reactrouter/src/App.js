cf9707b3a55b4ff6c87d8d9f01e2aceef92947d0
sthephanytezza-dev <sthephany.tezza@rethink.dev>
2022-05-16 11:42:29 -0300

    página 204

--- reactrouter/src/App.js
+++ reactrouter/src/App.js
@@ -8,6 +8,7 @@ import About from "./pages/About";
 import Navbar from "./components/Navbar";
 import Product from "./pages/Product";
 import Info from "./pages/Info";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/products/:id/info" element={<Info />} />
           <Route path="/products/:id" element={<Product />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
