758078565de50f40fac8fab3a59dcbfbf07509d7
sthephanytezza-dev <sthephany.tezza@rethink.dev>
2022-05-18 14:08:56 -0300

    criando um contexto mais complexo

---
+++ context-project/src/context/TitleColorContext.js
@@ -0,0 +1,19 @@
+import { createContext, useReducer } from "react";
+
+export const TitleColorContext = createContext();
+
+export const titleColorReduce = (state, action) => {
+  //switch
+};
+
+export const TitleColorContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(titleColorReduce, { color: "purple" });
+
+  console.log("Title color context: ", state);
+
+  return (
+    <TitleColorContext.Provider value={{ ...state }}>
+      {children}
+    </TitleColorContext.Provider>
+  );
+};
--- context-project/src/pages/Home.js
+++ context-project/src/pages/Home.js
@@ -8,13 +8,19 @@ import ChangeCounter from "../components/ChangeCounter";
 // 4- refatorando com o hook
 import { useCounterContext } from "../hooks/useCounterContext";
 
+// 5- context complexo
+import { useTitleColorContext } from "../hooks/useTitleColorContext";
+
 const Home = () => {
   // const { counter } = useContext(CounterContext);
   const { counter } = useCounterContext();
 
+  //5 - context mais complexo
+  const { color } = useTitleColorContext();
+
   return (
     <>
-      <h1>Home</h1>
+      <h1 style={{ color: color }}>Home</h1>
       <p>Valor do contador: {counter}</p>
       {/*3- Alterando valor do contexto */}
       <ChangeCounter />
