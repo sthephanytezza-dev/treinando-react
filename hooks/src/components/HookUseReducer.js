e4e50474a6ae0d840611bd8c86d56e2d30969589
sthephanytezza-dev <sthephany.tezza@rethink.dev>
2022-05-31 16:53:18 -0300

    useReducer com actions

--- hooks/src/components/HookUseReducer.js
+++ hooks/src/components/HookUseReducer.js
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
 
 const HookUseReducer = () => {
   // 1- começando com o useReducer
@@ -6,11 +6,62 @@ const HookUseReducer = () => {
     return Math.random(state);
   });
 
+  // 2- avançando no useReducer
+  const initialTasks = [
+    { id: 1, text: "Fazer alguma coisa" },
+    { id: 2, text: "Fazer outra coisa" },
+  ];
+
+  const taskReducer = (state, action) => {
+    switch (action.type) {
+      case "ADD":
+        const newTask = {
+          id: Math.random(),
+          text: taskText,
+        };
+        setTaskText("");
+
+        return [...state, newTask];
+      case "DELETE":
+        return state.filter((task) => task.id !== action.id);
+
+      default:
+        return state;
+    }
+  };
+
+  const [taskText, setTaskText] = useState("");
+  const [tasks, dispatchTasks] = useReducer(taskReducer, initialTasks);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    dispatchTasks({ type: "ADD" });
+  };
+
+  const removeTask = (id) => {
+    dispatchTasks({ type: "DELETE", id: id });
+  };
+
   return (
     <div>
       <h2>useReducer</h2>
       <p>Número: {number}</p>
       <button onClick={dispatch}>Alterar número</button>
+      <h3>Tarefas:</h3>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(e) => setTaskText(e.target.value)}
+          value={taskText}
+        />
+        <input type="submit" value="enviar" />
+      </form>
+      {tasks.map((task) => (
+        <li key={task.id} onDoubleClick={() => removeTask(task.id)}>
+          {task.text}
+        </li>
+      ))}
       <hr />
     </div>
   );
