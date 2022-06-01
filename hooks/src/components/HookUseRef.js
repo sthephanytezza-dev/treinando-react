0955cf9921866638443c3110658f22826a47eab1
sthephanytezza-dev <sthephany.tezza@rethink.dev>
2022-06-01 15:18:49 -0300

    useRef e DOM

--- hooks/src/components/HookUseRef.js
+++ hooks/src/components/HookUseRef.js
@@ -10,14 +10,38 @@ const HookUseRef = () => {
     numberRef.current = numberRef.current + 1;
   });
 
+  //2- useRef e DOM
+  const inputRef = useRef();
+  const [text, setText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setText("");
+
+    inputRef.current.focus();
+  };
+
   return (
     <div>
       <h2>useRef</h2>
+
+      {/* 1- useRef*/}
       <p>O componente renderizou: {numberRef.current} vezes.</p>
       <p>Counter 1: {counter}</p>
       <button onClick={() => setCounter(counter + 1)}>Contador A</button>
       <p>Counter 1: {counterB}</p>
       <button onClick={() => setCounterB(counterB + 1)}>Contador B</button>
+
+      {/* useRef e DOM */}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          ref={inputRef}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <input type="submit" value="enviar" />
+      </form>
       <hr />
     </div>
   );
