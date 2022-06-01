d6a69845912656ca2a12b77dc47e47a0565a2162
sthephanytezza-dev <sthephany.tezza@rethink.dev>
2022-06-01 15:03:24 -0300

    useContext

--- hooks/src/pages/About.js
+++ hooks/src/pages/About.js
@@ -1,5 +1,16 @@
+//useContext
+import { useContext } from "react";
+import { SomeContext } from "../components/HookUseContext";
+
 const About = () => {
-  return <div>About</div>;
+  const { contextValue } = useContext(SomeContext);
+
+  return (
+    <div>
+      <h2>About</h2>
+      <p>Valor do contexto: {contextValue}</p>
+    </div>
+  );
 };
 
 export default About;
--- hooks/src/pages/Home.js
+++ hooks/src/pages/Home.js
@@ -3,12 +3,21 @@ import HookUseReducer from "../components/HookUseReducer";
 import HookUseState from "../components/HookUseState";
 import styles from "./Home.module.css";
 
+//useContext
+import { useContext } from "react";
+import { SomeContext } from "../components/HookUseContext";
+
 const Home = () => {
+  const { contextValue } = useContext(SomeContext);
+
   return (
     <div>
       <HookUseState />
       <HookUseReducer />
       <HookUseEffect />
+      <h2>useContext</h2>
+      <p>Valor do contexto: {contextValue}</p>
+      <hr />
     </div>
   );
 };
